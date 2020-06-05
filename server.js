decf16f626c7d404cd3d758d1e02a174ef612085
fkhan698 <fkhan698@hotmail.com>
2020-06-05 17:30:28 -0500

    Restarted entire project from scratch

--- server.js
+++ server.js
@@ -1,33 +1,8 @@
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').config()
-}
+const express = require('express');
+const app = express();
 
-  const express = require('express')
-  const app = express()
-  const expressLayouts = require('express-ejs-layouts')
-  const bodyParser = require('body-parser')
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
 
-  const indexRouter = require('./routes/index')
-  const authorRouter = require('./routes/authors')
-
-  app.set('view engine', 'ejs')
-  app.set('views', __dirname + '/views')
-  app.set('layout', 'layouts/layout')
-  app.use(expressLayouts)
-  app.use(express.static('public'))
-  app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
-
-
-  const mongoose = require('mongoose')
-
-  const db = mongoose.connection
-
-  mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-  db.on('error', error => console.error(error))
-  db.once('open', () => console.log('Connected to Mongoose'))
-
-
-  app.use('/', indexRouter)
-  app.use('/authors', authorRouter)
-
-  app.listen(process.env.PORT || 3000)
+app.listen(3000)
