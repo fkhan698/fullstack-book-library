8ea62e6939e6734b4eff952a7e2315b20e0ff308
fkhan698 <fkhan698@hotmail.com>
2020-06-07 18:11:20 -0500

    Added express layouts

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ db.once('open', () => console.log('Connected to Mongoose'));
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
 app.use(express.static('public'))
 
 
