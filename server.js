4b2844b1a36e658d737ff3ca50da4cf9587a439d
fkhan698 <fkhan698@hotmail.com>
2020-06-07 18:01:49 -0500

    Setup views and database

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ db.once('open', () => console.log('Connected to Mongoose'));
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
-app.use(expressLayouts)
 app.use(express.static('public'))
 
 
