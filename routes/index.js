caf94eeb7eaae4ecea4d0194b7d9ddd690b1d5a2
fkhan698 <fkhan698@hotmail.com>
2020-06-07 12:29:26 -0500

    Setup views and database

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send("Authors page");
+    res.render('index')
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,31 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config({path: '.env'});
+}
+
 const express = require('express');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
+//Routers
+const authorRouter = require('./routes/author');
+const indexRouter = require('./routes/index')
+
+//Database connection
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
 })
+const db = mongoose.connection;
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'));
+
+//View engine
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('views')
+
+
+app.use('/', indexRouter);
+app.use('/authors', authorRouter)
 
 app.listen(3000)
