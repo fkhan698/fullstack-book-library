84d90381e326379dc4ae0f66f9e1dad2aa8089fc
fkhan698 <fkhan698@hotmail.com>
2020-06-14 10:42:41 -0500

    Removed multer and added filepond

--- routes/books.js
+++ routes/books.js
@@ -2,17 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Book = require('../models/book')
 const Author = require('../models/author')
-const multer = require('multer');
-const path = require('path');
-const uploadPath = path.join('public', Book.coverImageBasePath)
-const fs = require('fs')
 const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gifs']
-const upload = multer({
-    dest:  uploadPath,
-    fileFilter: (req, file, callback) => {
-        callback(null, imageMimeTypes.includes(file.mimetype))
-    }
-})
+
 
 
 router.get('/', async (req, res) => {
@@ -40,16 +31,16 @@ router.get('/new', async (req, res) => {
     renderNewPage(res, new Book())
 })
 
-router.post('/', upload.single('cover'), async (req, res) => {
-    const fileName = req.file != null ? req.file.filename : null
+router.post('/', async (req, res) => {
+
     let book  = new Book({
         title: req.body.title,
         author: req.body.author,
         publishDate: new Date(req.body.publishDate),
         pageCount: req.body.pageCount,
-        coverImageName: fileName,
         description: req.body.description
     })
+    saveCover(book, req.body.cover)
     try {
         book = await book.save();
         res.redirect(`books`)
@@ -72,5 +63,14 @@ async function renderNewPage(res, book, hasError = false){
         console.log(e)
         res.redirect('/books')
     }
+}
+function saveCover(book, coverEncoded){
+    if(coverEncoded == null) return
+    const cover = JSON.parse(coverEncoded)
+    if(cover != null && imageMimeTypes.includes(cover.type)){
+        book.coverImage = new Buffer.from(cover.data, 'base64')
+        book.coverImageType = cover.type
+    }
+
 }
 module.exports = router;
